5d64618f9d822010101dbaf495577a3c2b09cbeb
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2021-12-11 10:29:20 +0000

    Clarify usage of FindFirstAncestorOfClass

--- Character/Touched.lua
+++ Character/Touched.lua
@@ -3,6 +3,12 @@ local Players = game:GetService("Players")
 
 workspace.Baseplate.Touched:Connect(function(hit)
     -- Ancestor == Parent
+
+    -- normal parts don't have a model as their parent/parent's parent/etc so
+    -- FindFirstAncestorOfClass will return nil, whereas body parts will have
+    -- their parent be the character while accessories will have their parent's
+    -- parent be the character. If `playerChar` ends up being nil and it's
+    -- passed into `Players:GetPlayerFromCharacter()`, it'll just return nil
     local playerChar = hit:FindFirstAncestorOfClass("Model")
     local player = Players:GetPlayerFromCharacter(playerChar)
 
