1533294a640a7d6a8140eeae24bfcf828707ae71
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2023-12-28 05:25:36 +0000

    Elaborate on use case

--- Debounces/Numerical.lua
+++ Debounces/Numerical.lua
@@ -1,3 +1,8 @@
+-- these are beneficial normally in touched event handlers - because the
+-- event can fire many times at once, scopes are used to ensure that
+-- there is only one instance of your event handler running rather than
+-- many at a time. this race condition can only be solved via the below
+-- from my findings, though i have not confirmed other use cases
 local debounce = 0
 local example = workspace.Baseplate
 
