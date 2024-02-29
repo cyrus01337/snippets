56f9b7e730193aa17a41b5092db29994ef2d08bd
cyrus01337 <cyrus21337@gmail.com>
2024-02-29 16:44:29 +0000

    Minor expansion

--- Datastores/BindToClose.lua
+++ Datastores/BindToClose.lua
@@ -24,7 +24,7 @@ Players.PlayerRemoving:Connect(onPlayerRemoving)
 -- Note: this is only necessary in studio, as game servers normally ensure that
 -- functions connected to events run fine, though there may be some cases where
 -- saving player data takes so long that this may be necessary without the
--- studio check
+-- studio check (this normally isn't the case)
 if RunService:IsStudio() then
 	game:BindToClose(saveAllPlayersData)
 end
