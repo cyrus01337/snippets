72b8c949088fd6463594becd44bf5032f8243d1b
cyrus01337 <cyrus21337@gmail.com>
2024-02-29 16:44:02 +0000

    Expand on when to save player data during server teardown

--- Datastores/BindToClose.lua
+++ Datastores/BindToClose.lua
@@ -1,5 +1,6 @@
 -- https://create.roblox.com/docs/reference/engine/classes/DataModel#BindToClose
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
 local function onPlayerAdded(player)
 	-- Load player data here...
@@ -19,4 +20,11 @@ Players.PlayerAdded:Connect(onPlayerAdded)
 Players.PlayerRemoving:Connect(onPlayerRemoving)
 -- Whatever function we pass into BindToClose is run when the game is scheduled
 -- to shut down (meaning when 0 players are in the server)
-game:BindToClose(saveAllPlayersData)
+--
+-- Note: this is only necessary in studio, as game servers normally ensure that
+-- functions connected to events run fine, though there may be some cases where
+-- saving player data takes so long that this may be necessary without the
+-- studio check
+if RunService:IsStudio() then
+	game:BindToClose(saveAllPlayersData)
+end
